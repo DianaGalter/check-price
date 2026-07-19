4d2fcf0bd7281be7ce086cecf2f9f051175182e7
Diana Bobrova <diana.bobrova.spb@gmail.com>
2026-07-19 21:57:52 +0300

    create mocks

---
+++ src/entities/product/products.mock.ts
@@ -0,0 +1,33 @@
+import { type Product } from "./product";
+export const products: Product[] = [
+    {
+    "name": "Granite Trail Mid",
+    "price": 529.90,
+    "article": "2053201010",
+    "color": "black/ti grey"
+  },
+  {
+    "name": "Newton Wander Ltr",
+    "price": 699.90,
+    "article": "2126851012",
+    "color": "black/grill"
+  },
+  {
+    "name": "Newton Wander Ltr",
+    "price": 699.90,
+    "article": "2126851231",
+    "color": "cordovan/honey"
+  },
+  {
+    "name": "Newton Alpine Pt M",
+    "price": 759.90,
+    "article": "2128861005",
+    "color": "kettle/orange"
+  },
+  {
+    "name": "Crestwood Mid Waterproof",
+    "price": 549.90,
+    "article": "2100641313",
+    "color": "deep olive"
+  }
+];

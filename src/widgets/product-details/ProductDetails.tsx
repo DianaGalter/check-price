53d8edcc7a7fe0b914f71c41c60557857f8d4ca6
Diana Bobrova <diana.bobrova.spb@gmail.com>
2026-08-11 00:08:43 +0300

    update product details header styles

--- src/widgets/product-details/ProductDetails.tsx
+++ src/widgets/product-details/ProductDetails.tsx
@@ -23,7 +23,8 @@ export const ProductDetails = ({ product, onClose }: ProductDetailsProps) => {
             viewBox="0 0 24 24"
             aria-hidden="true"
           >
-            <path d="m15 18-6-6 6-6" />
+            <path d="M19 12H5" />
+            <path d="m11 18-6-6 6-6" />
           </svg>
         </button>
 

1bbb197dd020c140cb3339d721e8aa6c7a3a3b3a
Diana Bobrova <diana.bobrova.spb@gmail.com>
2026-08-12 23:52:52 +0300

    update mocks

--- src/entities/product/products.mock.ts
+++ src/entities/product/products.mock.ts
@@ -29,5 +29,221 @@ export const products: Product[] = [
     "price": 549.90,
     "article": "2100641313",
     "color": "deep olive"
+  },
+  {
+    "name": "Crestwood Mid Waterproof",
+    "price": 549.90,
+    "article": "2100641089",
+    "color": "d_grey/rusty"
+  },
+  {
+    "name": "Crestwood Waterproof M",
+    "price": 499.90,
+    "article": "2100651013",
+    "color": "black/grey"
+  },
+  {
+    "name": "Crestwood Waterproof M",
+    "price": 499.90,
+    "article": "2100651053",
+    "color": "graphite/dark adobe"
+  },
+  {
+    "name": "Peakfreak II Mid Outdry",
+    "price": 739.90,
+    "article": "2005091010",
+    "color": "black/titanium"
+  },
+  {
+    "name": "Peakfreak II Mid Outdry M",
+    "price": 739.90,
+    "article": "2100691010",
+    "color": "black/titanium"
+  },
+  {
+    "name": "Peakfreak II Mid Outdry M",
+    "price": 739.90,
+    "article": "2100691089",
+    "color": "dark grey/black"
+  },
+  {
+    "name": "Peakfreak II Outdry",
+    "price": 599.90,
+    "article": "2100711091",
+    "color": "moonstone/red"
+  },
+  {
+    "name": "Peakfreak II Outdry",
+    "price": 599.90,
+    "article": "2100711010",
+    "color": "black/shark"
+  },
+  {
+    "name": "Peakfreak II Outdry",
+    "price": 599.90,
+    "article": "2100711383",
+    "color": "nori/black"
+  },
+  {
+    "name": "Whipray M",
+    "price": 799.90,
+    "article": "2148981316",
+    "color": "cypress"
+  },
+  {
+    "name": "Whipray M",
+    "price": 799.90,
+    "article": "2148981012",
+    "color": "black/white"
+  },
+  {
+    "name": "Newton Nimble Ltr",
+    "price": 599.90,
+    "article": "2140371286",
+    "color": "elk/black"
+  },
+  {
+    "name": "Voyager Flx Pct M",
+    "price": 579.90,
+    "article": "2109401012",
+    "color": "black/blaze"
+  },
+  {
+    "name": "Firecamp Remesh",
+    "price": 469.90,
+    "article": "1826981010",
+    "color": "black/steam"
+  },
+  {
+    "name": "Firecamp Remesh",
+    "price": 469.90,
+    "article": "1826981096",
+    "color": "soft taupe"
+  },
+  {
+    "name": "Castback Tc PFG",
+    "price": 589.90,
+    "article": "2079411063",
+    "color": "grey ice"
+  },
+  {
+    "name": "Terrastride Aro",
+    "price": 449.90,
+    "article": "2115301010",
+    "color": "black/tusk"
+  },
+  {
+    "name": "Bonehead Lea",
+    "price": 499.90,
+    "article": "2132211010",
+    "color": "black/sea salt"
+  },
+  {
+    "name": "Bonehead Lea",
+    "price": 499.90,
+    "article": "2132211125",
+    "color": "sea salt"
+  },
+  {
+    "name": "Bonehead Lea",
+    "price": 499.90,
+    "article": "2132211200",
+    "color": "espresso ii/mud"
+  },
+  {
+    "name": "Konos Featherweight",
+    "price": 599.90,
+    "article": "2141611010",
+    "color": "black/white"
+  },
+  {
+    "name": "Konos Featherweight",
+    "price": 599.90,
+    "article": "2141611880",
+    "color": "mango/blue"
+  },
+  {
+    "name": "Konos Featherweight",
+    "price": 599.90,
+    "article": "2141611100",
+    "color": "white/grey ice"
+  },
+  {
+    "name": "Konos Flash",
+    "price": 599.90,
+    "article": "2185561010",
+    "color": "black/dark grey"
+  },
+  {
+    "name": "Konos TRS Outdry",
+    "price": 749.90,
+    "article": "2099811084",
+    "color": "cloud grey/black"
+  },
+  {
+    "name": "Konos TRS Outdry",
+    "price": 749.90,
+    "article": "2099811012",
+    "color": "black/grill"
+  },
+  {
+    "name": "Konos TRS Outdry",
+    "price": 749.90,
+    "article": "2099811120",
+    "color": "natural/black"
+  },
+  {
+    "name": "Konos TRS Outdry",
+    "price": 749.90,
+    "article": "2099811395",
+    "color": "grey green/black"
+  },
+  {
+    "name": "Konos Elevate M",
+    "price": 799.90,
+    "article": "2144011100",
+    "color": "white/grey"
+  },
+  {
+    "name": "Konos Elevate M",
+    "price": 799.90,
+    "article": "2144011089",
+    "color": "dark grey"
+  },
+  {
+    "name": "Konos Trillium ATR M",
+    "price": 799.90,
+    "article": "2145281010",
+    "color": "black/red"
+  },
+  {
+    "name": "Konos-Hiker-3-strap",
+    "price": 399.90,
+    "article": "2121571010",
+    "color": "black/grey"
+  },
+  {
+    "name": "Konos-Hiker-3-strap",
+    "price": 399.90,
+    "article": "2121571383",
+    "color": "nori/moss"
+  },
+  {
+    "name": "Breaksider sandal",
+    "price": 329.90,
+    "article": "2027191302",
+    "color": "mosstone/yellow"
+  },
+  {
+    "name": "Konos Globetrot M",
+    "price": 339.90,
+    "article": "2126931010",
+    "color": "black/white"
+  },
+  {
+    "name": "Konos Globetrot M",
+    "price": 339.90,
+    "article": "2126931028",
+    "color": "grill"
   }
 ];
